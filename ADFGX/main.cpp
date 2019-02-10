4f0782c45e5dc3c2ad968b6a019de35d1df4a2ba
Vladicka <vladicka7874@gmail.com>
2019-02-10 17:58:39 +0500

    Update main.cpp

--- ADFGX/main.cpp
+++ ADFGX/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 #include <stdlib.h>
 
 using namespace std;
+void random(char A[5][5]);
 string alphabet(string S, short int D[],short int k);
-void decrypt();
 
-char A[5][5]={{'r','f','e','v','g'},{'o','x','p','i','t'},{'k','q','b','y','w'},{'c','u','n','d','m'},{'j','l','s','a','h'}};
+char A[5][5]={{'a','b','c','d','e'},{'f','g','h','i','k'},{'l','m','n','o','p'},{'q','r','s','t','u'},{'v','w','x','y','z'}};
 short int D[10]={0,1,2,3,4,5,6,7,8,9};
-short int F[10]={0,1,2,3,4,5,6,7,8,9};
-short int G[10]={0,1,2,3,4,5,6,7,8,9};
 
 int main()
 {
+    srand((time(NULL)));
     string key, input, output, out;
     short int i,j,l,k,p=0,n;
     char B[5]={'A','D','F','G','X'};
@@ -21,14 +21,30 @@ int main()
     cout << "Enter your message: ";
     getline(std::cin, input);
     k=key.size();
-    for (i=0;i<input.size();i++){
-        if (input[i]==' ') input.erase(i,1);
-    }
+    //cout << k;
     l=input.size();
-    while (l % k !=0){
-        input.push_back(input[l-1]);
-        l++;
+    //cout << l;
+    for (i=0;i<l;i++){
+        if (input[i]==' '){
+            p++;
+            //cout << p;
+            for (j=i;j<l;j++){
+                input[j]=input[j+1];
+            }
+        }
     }
+    l=l-p;
+    //cout << p;
+    //cout << l;
+    p=l-1;
+    //cout << p;
+    while (l % k !=0){
+        input[l]=input[p];
+        l++;
+        if (l>input.size()) input.push_back (input[p]);
+    }
+    //cout << input;
+    random(A);
     for (i=0;i<5;i++){
         cout << "\n";
         for (j=0;j<5;j++){
@@ -39,16 +55,25 @@ int main()
         for (i=0;i<5;i++){
             for (j=0;j<5;j++){
                 if (input[p]==A[i][j]){
+                    //cout << B[i] << B[j];
                     out.push_back (B[i]);
                     out.push_back (B[j]);
+                    //output[2*k]=B[i];
+                    //output[2*k+1]=B[j];
                 }
             }
         }
     }
+    //for (i=0;i<2*l;i++){
+    //cout << "\n" << output;
+    //}
     key=alphabet(key,D,k);
     j=0;
+    //cout << l;
     for (i=0;i<2*l;i++){
         output.push_back (out[D[j]]);
+        //output[i]=out[B[j]];
+        //cout << output[i];
         j++;
         if (j==k){
             for (j=0;j<k;j++){
@@ -57,19 +82,45 @@ int main()
             j=0;
         }
     }
+    //cout << output;
+    //cout << key;
+    //for (i=0;i<k;i++){
+        //cout << D[i];
+    //}
     p=0;
     cout << "\n" << "\n";
+    //cout << l << "\n";
     for (i=0;i<k;i++){
+        //p++;
         for (n=0;n<(2*l)/k;n++){
+            //j=k*D[n]+i;
+            //cout << A[k] << " " << i << " ";
+            //cout << j << " ";
             cout << output[i+n*k];
+            //<< "(" << i << ")";
         }
         cout << " ";
+        //if (p==k){
+            //i=i+2*k;
+            //p=0;
+        //}
     }
-    cout << "\n";
-    decrypt();
     system("pause");
 }
-
+void random(char A[5][5])
+{
+    int i,j,k,l,m;
+    char c;
+    for (k=0;k<101;k++){
+        i=rand() % 5;
+        j=rand() % 5;
+        l=rand() % 5;
+        m=rand() % 5;
+        c=A[i][j];
+        A[i][j]=A[l][m];
+        A[l][m]=c;
+    }
+}
 string alphabet(string S, short int D[],short int k){
     short int i,j,a;
     char c;
@@ -86,39 +137,4 @@ string alphabet(string S, short int D[],short int k){
         }
     }
     return(S);
-}
-
-void decrypt(){
-    char B[5]={'A','D','F','G','X'};
-    string input,key,out,output;
-    short int i,j=0,k,l,n,q;
-    cout << "Enter encrypted message: ";
-    getline(cin,input);
-    for (i=0;i<input.size();i++){
-        if (input[i]==' ') input.erase(i,1);
-    }
-    cout << "Enter your key: ";
-    cin >> key;
-    k=key.size();
-    l=input.size();
-    q=l/k;
-    key=alphabet(key,F,k);
-    for (i=0;i<k;i++){
-        G[F[i]]=i;
-    }
-    for (i=0;i<q;i++){
-        for (j=0;j<k;j++){
-            output.push_back(input[q*G[j]+i]);
-        }
-    }
-    i=0;
-    j=0;
-    for (n=0;n<l;n=n+2){
-        for (k=0;k<5;k++){
-            if (output[n]==B[k]) i=k;
-            if (output[n+1]==B[k]) j=k;
-        }
-        cout << A[i][j];
-    }
-    cout << "\n";
 }
